8599ca814f5963883252d790903c8bd6e0fdc850
DANILO CUICHAN <cdcuichan@espe.edu.ec>
2021-11-25 09:54:05 -0500

    carga

---
+++ Parte III/validar.js
@@ -0,0 +1,28 @@
+function validar() {
+	// body...
+	var correo, telefono, expresion;
+	correo= document.getElementById('txtEmail').value;
+	telefono= document.getElementById('txtTelefono').value;
+	expresion= /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
+	
+	if(!expresion.test(correo)){
+		alert("El correo no es válido");
+		return false;
+	}
+	
+	else if(telefono.length!=10){
+		alert("El telefono es INCORRECTO");
+		return false;
+	}
+	else if(isNaN(telefono)){
+		alert("El telefono ingresado no es un número");
+		return false;
+	}
+	
+}
+function checkSubmit() {
+    document.getElementById("btnEnviar").value = "Enviando...";
+    //alert("Datos enviados correctamente");
+    document.getElementById("btnEnviar").disabled = true;
+    return true;
+}
